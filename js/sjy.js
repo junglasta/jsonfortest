ad0aaa9054c35d83ab374686bc65c29d83295ff4
jung_la_sta <jung_la_sta@naver.com>
2022-11-22 14:49:01 +0900

    자소서상단

---
+++ js/sjy.js
@@ -0,0 +1,40 @@
+// 제이쿼리 GET방식으로 ajax 부르기 (자바스크립트는 넘 복잡)
+$(function(){
+    $.ajax({
+        type : "GET",
+        url : "/pwa/data/navi.json", // 서버 절대 경로!
+        datatType : "json",
+        success : function( naviData ){
+            /////////비동기통신 데이터 역할은 여기까기  
+            // ul
+            var gnbli = '';
+            $.each(naviData, (index, item)=>{
+                if( item.isNav ){ // json에서 노출여부 설정
+                    gnbli += `<li class="nav-item ${item.navClass}">
+                                <a class="nav-link" href="${item.navLink}">${item.navTitle}
+                                </a>
+                            </li>`;
+                }                
+            })
+            $('#gnb').html(gnbli)
+            /////////비동기통신 데이터 역할은 여기까기 마침
+
+            /////////여기서부터는 인터렉티브작업이 시작됨
+            $('#gnb').on('click', '.scrollPage a', function(e){
+                e.preventDefault();
+                var _this = $(this) // 클릭한 객체의 a를 저장 / $(this)가 어떤 이벤트에 쓰이느냐에 따라 다르니까, 변수 지정
+                $('body, html').animate({
+                    scrollTop: $($(this).attr('href')).offset().top
+                                },400, function(){
+                        // 애니메이션 끝나고 처리될 실행문
+                        _this.parent().addClass('active').siblings().removeClass('active')
+                });
+            });
+        },
+        error: function (xhr, status, e) { // 여기는 그냥 복붙
+            console.log("error", e); // 에러메세지가 세번째인자
+            console.log("status", status); // 상태가 2번째인자
+        }
+    });
+    // 비동기통신과 상관없는 작업은 여기에 해야 최적화를 고려한 작업이 됩니다
+})
